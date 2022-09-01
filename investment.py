e1a2adb2633de7185248eb184a966f30bd9610ac
decoricky <rikiya.shikida@gmail.com>
2022-09-01 18:20:20 +0900

    argument parser

--- investment.py
+++ investment.py
@@ -1,11 +1,14 @@
+import argparse
+
+
 def calc_regular_investment_result(
     investment_amount_per_month: int = 50000,
-    investment_range_year: int = 20,
+    investment_period_year: int = 20,
     return_on_investment_percent: int = 5,
 ):
     investment_amount_per_year = investment_amount_per_month * 12
     simple_result, regular_result = 0, 0
-    for i in range(investment_range_year):
+    for i in range(investment_period_year):
         simple_result += investment_amount_per_year
         regular_result += investment_amount_per_year
         regular_result *= 1 + return_on_investment_percent / 100
@@ -14,14 +17,21 @@ def calc_regular_investment_result(
 
 
 def calc_regular_investment_amount(
-    investment_range_year: int = 10,
+    investment_period_year: int = 10,
     target_amount: int = 100_000_000,
     return_on_investment_percent: int = 5,
 ):
     pass
 
 
-if __name__ == "__main__":
-    calc_regular_investment_result()
-    calc_regular_investment_result(investment_amount_per_month=100000)
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--investment-amount-per-month", help="investment amount per month", type=int, required=True)
+parser.add_argument("-p", "--investment-period-year", help="investment period year", type=int, required=True)
+parser.add_argument("-r", "--return-on-investment-percent", help="return on investment percent", type=int, required=True)
+args = parser.parse_args()
+
+result = calc_regular_investment_result(
+    args.investment_amount_per_month, args.investment_period_year, args.return_on_investment_percent
+)
+print(result)
 
